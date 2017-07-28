46bfaded799dfa04255b87f331415d68d9e98bdb
Luke-mc <luke_mcglynn@yahoo.com>
2017-07-27 19:25:03 -1000

    finished 10

--- joins.sql
+++ joins.sql
@@ -1,17 +1,45 @@
-
+--1
 -- SELECT * FROM users;
-
+--2
 -- SELECT * FROM posts
 -- WHERE users_id = 100;
-
+--3
 -- SELECT *, users.first_name, users.last_name
 -- FROM posts
 -- INNER JOIN users ON posts.users_id =  users.id
 -- WHERE posts.users_id = 200
 -- AND users.id = 200;
-
-SELECT posts.*, users.username
-FROM posts
-INNER JOIN users ON posts.users_id = users.id
-WHERE users.first_name = 'Norene'
-AND users.last_name = 'Schmitt';
+--4
+-- SELECT posts.*, users.username
+-- FROM posts
+-- INNER JOIN users ON posts.users_id = users.id
+-- WHERE users.first_name = 'Norene'
+-- AND users.last_name = 'Schmitt';
+--5
+-- SELECT username
+-- FROM users
+-- WHERE created_at < '2015-01-01';
+--6
+-- SELECT posts.title, posts.content, users.username
+-- FROM posts
+-- INNER JOIN users ON posts.users_id = users.id
+-- WHERE users.created_at < '2015-01-01';
+-- 7
+-- SELECT comments.*, posts.title AS "Post Title"
+-- FROM comments
+-- INNER JOIN posts ON posts.id = comments.posts_id
+--8
+-- SELECT comments.*, posts.title AS "post_title", posts.url AS "post_url", comments.body AS "comment_body"
+-- FROM comments
+-- INNER JOIN posts ON posts.id = comments.posts_id
+-- WHERE posts.created_at < '2015-01-01';
+--9
+-- SELECT comments.*, posts.title AS "post_title", posts.url AS "post_url", comments.body AS "comment_body"
+-- FROM comments
+-- INNER JOIN posts ON posts.id = comments.posts_id
+-- WHERE posts.created_at > '2015-01-01';
+--10
+-- SELECT comments.*, posts.title AS "post_title", posts.url AS "post_url", comments.body AS "comment_body"
+-- FROM comments
+-- INNER JOIN posts ON posts.id = comments.posts_id
+-- WHERE comments.body LIKE '%USB%';
